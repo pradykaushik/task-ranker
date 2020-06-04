c3e33e4645691712b22b8c77aacbbc8db5e5c187
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2020-06-04 00:23:19 -0400

    formatted code

--- ranker.go
+++ ranker.go
@@ -2,15 +2,15 @@
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-* 
+*
 * http://www.apache.org/licenses/LICENSE-2.0
-* 
+*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
-*/
+ */
 
 package taskranker
 
@@ -21,9 +21,9 @@ import (
 )
 
 type TaskRanker struct {
-	PrometheusEndpoint string `yaml:"prometheus_endpoint"`
-	Strategy string `yaml:"strategy"`
-	FilterLabels []string `yaml:"filter_labels"`
+	PrometheusEndpoint string   `yaml:"prometheus_endpoint"`
+	Strategy           string   `yaml:"strategy"`
+	FilterLabels       []string `yaml:"filter_labels"`
 }
 
 func New(options ...Option) (*TaskRanker, error) {
@@ -38,9 +38,9 @@ func New(options ...Option) (*TaskRanker, error) {
 
 func (tRanker *TaskRanker) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	type tempRanker struct {
-		PrometheusEndpoint string `yaml:"prometheus_endpoint"`
-		Strategy string `yaml:"strategy"`
-		FilterLabels []string `yaml:"filter_labels"`
+		PrometheusEndpoint string   `yaml:"prometheus_endpoint"`
+		Strategy           string   `yaml:"strategy"`
+		FilterLabels       []string `yaml:"filter_labels"`
 	}
 
 	t := new(tempRanker)
