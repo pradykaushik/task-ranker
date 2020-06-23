22420b8952a3149bd1a23f7eb88be61d4dae2d24
Pradyumna Kaushik <pkaushi1@binghamton.edu>
2020-06-22 22:26:25 -0400

    update test function name.

--- query/builder_test.go
+++ query/builder_test.go
@@ -40,7 +40,7 @@ func TestMain(m *testing.M) {
 	m.Run()
 }
 
-func TestGetBuilder(t *testing.T) {
+func TestNewBuilder(t *testing.T) {
 	assert.NotNil(t, testBuilder)
 	assert.Equal(t, "test_metric", testBuilder.metric)
 	assert.Len(t, testBuilder.labelMatchers, 2)
